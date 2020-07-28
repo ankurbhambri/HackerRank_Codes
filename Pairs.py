3b330a8205e9aff64dc46aebd065c7f1de460d5b
Ankur Bhambri <ankur.bhambri@renewbuy.com>
2020-07-28 13:00:17 +0530

    changes

--- Pairs.py
+++ Pairs.py
@@ -1,9 +1,18 @@
 from itertools import combinations
 
 def pairs(k, arr):
+    import ipdb; ipdb.set_trace()
     comb = list(combinations(arr, 2))
     pairs = list(filter(lambda x: abs(x[1] - x[0]) == k, comb))
     return len(pairs)
+
+    # trick
+
+    a = set(arr)
+    # make a set of all a[i] + k
+    b = set(x + k for x in arr)
+    # return the length of the intersection set
+    return len(a&b)
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
