a309c9e75d181fee45cefd4d954c13a3c1f2f156
Ankur Bhambri <ankur.bhambri@renewbuy.com>
2020-07-31 16:23:01 +0530

    new code

---
+++ carry_forward.py
@@ -0,0 +1,25 @@
+
+def given_array(given_list, n):
+    carry = 1 
+    for i in reversed(range(n)):
+
+        if (given_list[i] + 1) >= 10:
+            given_list[i] = given_list[i] + carry - 10
+
+            if i==0 and given_list[i] == 0:
+                given_list.insert(0, 1)
+        else:
+            given_list[i] += 1
+            return given_list
+
+    return given_list
+
+print('Enter the length of Array')
+ll = int(input())
+
+val = [int(input()) for k in range(ll)]
+print('Your array', val)
+
+val = given_array(val, ll)
+
+print(val)
